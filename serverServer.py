8b81a7c0d1522ae508ce0988e4af08a595b9e561
Abhinav parbhakar <abhinav.parbhakar302@gmail.com>
2023-07-16 15:30:58 -0600

    pushing to the pi

--- serverServer.py
+++ serverServer.py
@@ -21,6 +21,9 @@ def reset_servos():
 def send_servo_signal(message:str):
     messageList = message.split("/") #splits on the "/" to give the angle change in [0] and in axis in [1]
     angle_change = int(messageList[0])
+    print("[X-ANGLE]: {}".format(servo_x_angle))
+    print("[Y-ANGLE]: {}".format(servo_y_angle))
+    print(["[CHANGE-ANGLE]: {}".format(angle_change)])
     match messageList[1]:
         case "x":
             servo_x_angle += angle_change
